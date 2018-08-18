055d66b41be3665b4c6580666e3a09fea3dea4fd
Monica  Karasek <mlkarasek@gmail.com>
2018-08-18 09:29:29 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,7 +4,7 @@ describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
-  end 
+  end
 
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
     visit new_post_path
